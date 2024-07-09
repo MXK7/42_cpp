8dbdc0b50aaf52459d1cda11fc961302894bf380
MXK7 <medixom@gmail.com>
2024-07-09 19:46:23 +0200

    add cpp01/ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,54 +3,42 @@
 #include <sstream>
 #include <string>
 
-void	replaceStringInFile(const std::string &filename, const std::string &s1,
-		const std::string &s2)
-{
-	size_t	pos;
+// void replaceStringInFile(const std::string &filename, const std::string &s1, const std::string &s2)
 
-	std::ifstream fileIn(filename.c_str());
-	if (!fileIn.is_open())
-	{
-		throw std::runtime_error("Cannot open the file: " + filename);
-	}
-	std::stringstream buffer;
-	buffer << fileIn.rdbuf();
-	std::string content = buffer.str();
-	fileIn.close();
-	pos = 0;
-	while ((pos = content.find(s1, pos)) != std::string::npos)
-	{
-		content = content.substr(0, pos) + s2 + content.substr(pos + s1.length());
-		pos += s2.length();
-	}
-	std::ofstream fileOut((filename + ".replace").c_str());
-	if (!fileOut.is_open())
-	{
-		throw std::runtime_error("Cannot open the file to write: " + filename + ".replace");
-	}
-	fileOut << content;
-	fileOut.close();
+void return_error(int argc, const std::string &filename, const std::string &s1, const std::string &s2)
+{
+    if (argc != 4)
+        std::cerr << "Usage: " << filename << " <filename> <string1> <string2>\n";
+    else if (filename.empty() || s1.empty() || s2.empty())
+        std::cerr << "Error: Empty string is not allowed.\n";
+    else if (s1 == s2)
+        std::cerr << "Error: The strings are the same.\n";
+    return;
 }
 
-int	main(int argc, char **argv)
+void copy_folder(std::string filename, std::string filenameDest)
 {
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " <filename> <string1> <string2>\n";
-		return (1);
-	}
-	std::string filename = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-	try
-	{
-		replaceStringInFile(filename, s1, s2);
-		std::cout << "Replacement completed successfully.\n";
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-		return (1);
-	}
-	return (0);
+	std::ifstream src(filename.c_str());
+    if (!src.is_open())
+        std::cerr << "Error: Unable to open the source file " << filename << std::endl;
+    std::ofstream dest(filenameDest.c_str());
+    if (!dest.is_open())
+        std::cerr << "Error: Unable to open the destination file " << filenameDest << std::endl;
+    src.close();
+    dest.close();
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+    std::string filename = argv[1];
+    std::string filenameDest = filename + ".replace";
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    return_error(argc, filename, s1, s2);
+	copy_folder(filename, filenameDest);
+	// TODO: ADD COPY AND REPLACE STRING 
+
+    return 0;
 }
