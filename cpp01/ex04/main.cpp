5c3dfbbc9040d03ff35b1c3ae8061d37ec04fcb7
MXK7 <medixom@gmail.com>
2024-07-10 17:36:27 +0200

    add

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,8 +3,6 @@
 #include <sstream>
 #include <string>
 
-// void replaceStringInFile(const std::string &filename, const std::string &s1, const std::string &s2)
-
 void return_error(int argc, const std::string &filename, const std::string &s1, const std::string &s2)
 {
     if (argc != 4)
@@ -16,7 +14,7 @@ void return_error(int argc, const std::string &filename, const std::string &s1, 
     return;
 }
 
-void copy_folder(std::string filename, std::string filenameDest)
+void copy_folder(std::string filename, std::string filenameDest, std::string s1, std::string s2)
 {
 	std::ifstream src(filename.c_str());
     if (!src.is_open())
@@ -24,6 +22,17 @@ void copy_folder(std::string filename, std::string filenameDest)
     std::ofstream dest(filenameDest.c_str());
     if (!dest.is_open())
         std::cerr << "Error: Unable to open the destination file " << filenameDest << std::endl;
+    std::string line;
+    while (std::getline(src, line))
+    {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            line.replace(pos, s1.length(), s2);
+            pos += s2.length();
+        }
+        dest << line << std::endl;
+    }
     src.close();
     dest.close();
 	return ;
@@ -37,8 +46,7 @@ int main(int argc, char **argv)
     std::string s2 = argv[3];
 
     return_error(argc, filename, s1, s2);
-	copy_folder(filename, filenameDest);
-	// TODO: ADD COPY AND REPLACE STRING 
+	copy_folder(filename, filenameDest, s1, s2);
 
     return 0;
 }
