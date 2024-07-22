586baa62c6ebf9d853d503da144765eab1a1328e
MXK7 <medixom@gmail.com>
2024-07-22 11:37:54 +0200

    finish cpp02

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -31,11 +31,119 @@ Fixed::~Fixed(void)
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+    std::cout << "Assignation \"=\" operator called" << std::endl;
     if (this != &other)
-        this->raw = other.getRawBits();
+        this->raw = other.raw;
     return *this;
 }
+
+
+
+bool Fixed::operator>(const Fixed &other) const
+{
+    std::cout << "Assignation \">\" operator called" << std::endl;
+    return this->raw > other.raw;
+}
+
+bool Fixed::operator<(const Fixed &other) const
+{
+    std::cout << "Assignation \"<\" operator called" << std::endl;
+    return this->raw < other.raw;
+}
+
+bool Fixed::operator>=(const Fixed &other) const
+{
+    std::cout << "Assignation \">=\" operator called" << std::endl;
+    return this->raw >= other.raw;
+}
+
+bool Fixed::operator<=(const Fixed &other) const
+{
+    std::cout << "Assignation \"<=\" operator called" << std::endl;
+    return this->raw <= other.raw;
+}
+
+bool Fixed::operator==(const Fixed &other) const
+{
+    std::cout << "Assignation \"==\" operator called" << std::endl;
+    return this->raw == other.raw;
+}
+
+bool Fixed::operator!=(const Fixed &other) const
+{
+    std::cout << "Assignation \"!=\" operator called" << std::endl;
+    return this->raw != other.raw;
+}
+
+Fixed Fixed::operator+(const Fixed &other) const
+{
+    Fixed tmp;
+
+    tmp.raw = this->raw + other.raw;
+    std::cout << "Assignation \"+\" operator called" << std::endl;
+    return tmp;
+}
+
+Fixed Fixed::operator-(const Fixed &other) const
+{
+    Fixed tmp;
+
+    tmp.raw = this->raw - other.raw;
+    std::cout << "Assignation \"-\" operator called" << std::endl;
+    return tmp;
+}
+
+Fixed Fixed::operator*(const Fixed &other) const
+{
+    Fixed tmp;
+
+    tmp.raw = (this->raw * other.raw) >> Fixed::bits;
+    std::cout << "Assignation \"*\" operator called" << std::endl;
+    return tmp;
+}
+
+Fixed Fixed::operator/(const Fixed &other) const
+{
+    Fixed tmp;
+
+    tmp.raw = (this->raw << Fixed::bits) / other.raw;
+    std::cout << "Assignation \"/\" operator called" << std::endl;
+    return tmp;
+}
+
+Fixed &Fixed::operator++(void)
+{
+    this->raw++;
+    std::cout << "Assignation \"++\" operator called" << std::endl;
+    return *this;
+}
+
+Fixed Fixed::operator++(int)
+{
+    Fixed tmp(*this);
+
+    this->raw++;
+    std::cout << "Assignation \"++\" operator called" << std::endl;
+    return tmp;
+}
+
+Fixed &Fixed::operator--(void)
+{
+    this->raw--;
+    std::cout << "Assignation \"--\" operator called" << std::endl;
+    return *this;
+}
+
+Fixed Fixed::operator--(int)
+{
+    Fixed tmp(*this);
+
+    this->raw--;
+    std::cout << "Assignation \"--\" operator called" << std::endl;
+    return tmp;
+}
+
+
 
 int Fixed::getRawBits(void) const
 {
@@ -49,34 +157,51 @@ void Fixed::setRawBits(int const raw)
     this->raw = raw;
 }
 
+
+
 float Fixed::toFloat(void) const
 {
+    std::cout << "toFloat member function called" << std::endl;
     return this->raw / static_cast<float>(1 << Fixed::bits);
 }
 
 int Fixed::toInt(void) const
 {
+    std::cout << "toInt member function called" << std::endl;
     return this->raw >> Fixed::bits;
 }
 
+
+
 std::ostream &operator<<(std::ostream &os, const Fixed &obj)
 {
+    std::cout << "Assignation \"<<\" operator called" << std::endl;
     os << obj.toFloat();
     return os;
 }
 
-const Fixed     &Fixed::min(Fixed &a, Fixed &b) {
+
+
+const Fixed     &Fixed::min(Fixed &a, Fixed &b)
+{
+    std::cout << "Assignation \"min\" operator called" << std::endl;
     return a < b ? a : b;
 }
 
-const Fixed     &Fixed::min(const Fixed &a, const Fixed &b) {
+const Fixed     &Fixed::min(const Fixed &a, const Fixed &b)
+{
+    std::cout << "Assignation \"min\" operator called" << std::endl;
     return a < b ? a : b;
 }
 
-const Fixed     &Fixed::max(Fixed &a, Fixed &b) {
+const Fixed     &Fixed::max(Fixed &a, Fixed &b)
+{
+    std::cout << "Assignation \"max\" operator called" << std::endl;
     return a > b ? a : b;
 }
 
-const Fixed     &Fixed::max(const Fixed &a, const Fixed &b) {
+const Fixed     &Fixed::max(const Fixed &a, const Fixed &b)
+{
+    std::cout << "Assignation \"max\" operator called" << std::endl;
     return a > b ? a : b;
 }
