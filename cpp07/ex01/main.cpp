2f699b5782c6a130610582ccd147cda94a5656e2
Mehdi <47466257+MXK7@users.noreply.github.com>
2025-03-20 12:21:06 +0100

    finish

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int			intArr[] = {4, 2, 2, 1};
-	std::string const strArr[] = {"Comment", "ca", "mon", "reuf", "?", "pilou", "?!!"};
+	std::string const strArr[] = {"Nouilles", "bande", "de", "dwidwin", "?", "hehe", "coucou"};
 
 	iter(intArr, 4, print);
 	std::cout << std::endl;
